28019486140a3049d13de5b94a0128602846da9b
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-08-29 20:42:10 -0300

    Header Menu

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Main",
+    // component: () => import('../views/Main/Main'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/gallery",
+    name: "Gallery",
+    // component: () => import('../views/Main/Main'),
+  },
+  {
+    path: "/projects",
+    name: "Projects",
+    // component: () => import('../views/Main/Main'),
+  },
+  {
+    path: "/certifications",
+    name: "Certifications",
+    // component: () => import('../views/Main/Main'),
+  },
+  {
+    path: "/contacts",
+    name: "Contacts",
+    // component: () => import('../views/Main/Main'),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
