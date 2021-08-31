62686a6ef1c5e94467d8275b6d525ddb6ed0d3f5
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-08-31 05:49:25 -0300

    Card projects

---
+++ src/components/projects/CardProject.vue
@@ -0,0 +1,89 @@
+<template>
+  <section class="card_project">
+    <div
+      class="card_project_image"
+      :style="{ backgroundImage: 'url(' + project.image + ')' }"
+    />
+    <div class="card_project_content">
+      <h2 class="card_project_title">
+        {{ project.title }}
+      </h2>
+      <p class="card_project_description">
+        {{ project.description }}
+      </p>
+      <button-action
+        class="card_project_action"
+        label="View More"
+        :click="viewMore"
+        dark
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import ButtonAction from "@/components/shared/ButtonAction";
+
+export default {
+  name: "CardProject",
+
+  components: {
+    ButtonAction,
+  },
+
+  props: {
+    project: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  methods: {
+    viewMore() {},
+  },
+};
+</script>
+
+<style lang="scss">
+.card_project {
+    background: #fbfbfb;
+    display: flex;
+    flex-direction: row;
+    .card_project_image {
+        flex: 1;
+        width: 100%;
+        height: inherit;
+        background-size: cover;
+        background-position: center;
+    }
+    .card_project_content {
+        flex: 1.5;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        font-family: "Roboto", sans-serif;
+        padding: 30px;
+        .card_project_title {
+            font-size: 40px;
+            font-weight: 300;
+            color: #bdbdbd;
+        }
+        .card_project_description {
+            font-weight: 300;
+            color: #000000;
+            line-height: 25px;
+            margin: 25px 0;
+        }
+    }
+}
+
+@media only screen and (max-width: 768px) {
+    .card_project {
+        flex-direction: column;
+        .card_project_image {
+            flex: none;
+            height: 300px;
+        }
+    }
+}
+</style>
--- src/views/projects/index.vue
+++ src/views/projects/index.vue
@@ -9,13 +9,22 @@
       class="projects__divider"
       type="horizontal"
     />
+    <div class="projects__container">
+      <card-project
+        v-for="project in projects"
+        :key="project.id"
+        class="projects__item"
+        :project="project"
+      />
+    </div>
   </main>
 </template>
 
 <script>
 import SectionTitle from "@/components/shared/SectionTitle";
 import Divider from "@/components/shared/Divider";
-import ScrollReveal from "scrollreveal";
+import CardProject from "@/components/projects/CardProject";
+import ScrollReveal from "scrollreveal"; 
 
 export default {
   name: "Projects",
@@ -23,6 +32,32 @@ export default {
   components: {
     SectionTitle,
     Divider,
+    CardProject,
+  },
+
+  data() {
+    return {
+      projects: [
+        {
+          id: 1,
+          title: 'Sample Project 1',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          image: 'https://i.imgur.com/oUmU7cb.jpg',
+        },
+        {
+          id: 2,
+          title: 'Sample Project 2',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          image: 'https://i.imgur.com/eJJRwo2.jpg',
+        },
+        {
+          id: 3,
+          title: 'Sample Project 3',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          image: 'https://i.imgur.com/k9oF6Ok.jpg',
+        },
+      ]
+    }
   },
 
   mounted() {
@@ -31,14 +66,19 @@ export default {
 
   methods: {
     scrollReveal() {
-      let sr = ScrollReveal({
+      ScrollReveal({
         origin: "top",
         distance: "50px",
         duration: 2000,
         reset: true,
-      });
+      }).reveal(".projects__title", { delay: 200 });
 
-      sr.reveal(".projects__title", { delay: 200 });
+      ScrollReveal({
+        origin: "left",
+        distance: "50px",
+        duration: 2000,
+        reset: true,
+      }).reveal(".projects__item", { delay: 200 })
     },
   },
 };
@@ -51,6 +91,12 @@ export default {
   padding: 0 10px;
   .projects__title {
     margin: 60px 0;
+  }
+  .projects__container {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    margin-bottom: 150px;
   }
 }
 
